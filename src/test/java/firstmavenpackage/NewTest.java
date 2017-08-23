c2e6bd422319a19ea1e419a00022c2ae36fcdf0d
preetishbhanja <preetish.bhanja@gmail.com>
2017-08-23 20:47:22 +0530

    this is commited

---
+++ src/test/java/firstmavenpackage/NewTest.java
@@ -0,0 +1,21 @@
+package firstmavenpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  
+@Test
+  public void facebook() {
+	  
+WebDriver driver=new FirefoxDriver();
+	  driver.manage().window().maximize();
+driver.get("http://facebook.com");
+
+driver.findElement(By.id("email")).sendKeys("preetishbhanja@gmail.com");
+driver.findElement(By.id("pass")).sendKeys("gudubabu");
+driver.findElement(By.cssSelector("input[type='submit']")).click();
+}
+}
